48f5db8abb986ee9e441e3458adb7587ecc7a889
stcy2828 <stcy2828@gmail.com>
2026-02-24 17:29:44 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
+    
     return {
+      // 【最關鍵修正】
+      // 如果你的網址是 https://stcy2828.github.io/GMusicWebsite/
+      // 這裡必須填寫你的 Repository 名稱，前後加上斜線
+      base: '/GMusicWebsite/', 
+
       server: {
         port: 3000,
         host: '0.0.0.0',
